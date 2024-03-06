7b8b60bd2a29dcfe1ac22013247ed4564c2d13d5
Riste Ristov <riste@digitalnode.com>
2024-03-06 22:17:23 +0200

    Added messages decryption

--- src/components/ChatPage/ChatBox/MessageList.tsx
+++ src/components/ChatPage/ChatBox/MessageList.tsx
@@ -2,19 +2,44 @@ import React, {useCallback, useEffect, useState} from 'react';
 import './MessageList.css';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import {useSelector, useDispatch} from 'react-redux';
+import { decryptData } from '../../../helpers/cryptography';
+import secureStorage from 'react-secure-storage';
 
 
 const MessageList = () => {
-  const messages = useSelector((state:any) => state.chat.messages);
+  const [decryptedMessages, setDecryptedMessages] = useState([]);
+  const messages = useSelector((state:any) => { return state.chat.messages; });
   const chatMode:number = useSelector((state:any) => state.channel.mode);
   const currentChannel = useSelector((state:any) => state.channel.channel);
   const receiverId = useSelector((state:any) => state.channel.directUserId);
   const userId = useSelector((state:any) => state.login.user_id);
   const directUserName = useSelector((state:any) => state.channel.directUserName);
+  const userKey: any  = secureStorage.getItem(`${userId}:keyPair`) as object;
+
+  async function decryptMessages(messages: any) {
+    let messagesToReturn: any = [];
+    // @ts-ignore
+    for (let i = 0; i < messages.length; i++) {
+      messagesToReturn.push({...messages[i]});
+      if(messages[i].receiverId == userId && chatMode == 1) {
+        messagesToReturn[i].content = await decryptData(messages[i].content_reciever, userKey.privateKey);
+      } else if(messages[i].senderId == userId && chatMode == 1) {
+        messagesToReturn[i].content = await decryptData(messages[i].content_sender, userKey.privateKey);
+      }
+    }
+
+    return messagesToReturn;
+  }
 
   useEffect(() => {
-    
-  },[chatMode, currentChannel]);
+    decryptMessages(messages).then((data) => {
+      setDecryptedMessages(data);
+      console.log(data);
+    }).catch((e) => {
+      console.log(e)
+    })    
+  },[chatMode, currentChannel, messages]);
+
 
   return (
     <>
@@ -27,7 +52,7 @@ const MessageList = () => {
       {chatMode === 1 && 
         <>
           <div className="message_list">
-            {messages && messages
+            {decryptedMessages && decryptedMessages
             .filter((m:any) => 
               m.chatMode === 1 && 
               ((userId === m.receiverId && m.senderId === receiverId) ||
@@ -45,7 +70,7 @@ const MessageList = () => {
       {chatMode === 2 && 
         <>
           <div className="message_list">
-            {messages && messages.filter((m:any) => m.channelId === currentChannel.channelId && m.chatMode === 2).map((message:any) => (
+            {decryptedMessages && decryptedMessages.filter((m:any) => m.channelId === currentChannel.channelId && m.chatMode === 2).map((message:any) => (
               <div key={message.messageId} className="message">
                 <strong 
                 >{message.senderName}</strong>: {message.content}
